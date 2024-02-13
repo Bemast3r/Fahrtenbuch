26dd7c3f4293d32cc4be813eb58b681b941af83b
s88475 <s88475@bht-berlin.de>
2024-02-13 18:08:23 +0100

    Resources hinzugefügt

--- backend/src/db/Resources.ts
+++ backend/src/db/Resources.ts
@@ -13,6 +13,20 @@ export type UserResource = {
     }[];
 }
 
+export type FahrtResource = {
+    fahrer: string; // ID des Fahrers
+    kennzeichen: string; // Kennzeichen des Fahrzeugs
+    kilometerstand: number;
+    kilometerende: number;
+    lenkzeit?: number; // Zeit, die der Fahrer am Steuer verbracht hat (in Minuten z.B.)
+    arbeitszeit?: number; // Gesamte Arbeitszeit des Fahrers (in Minuten z.B.)
+    pause?: number; // Dauer der Pause des Fahrers (in Minuten z.B.)
+    createdAt?: Date; // Datum der Fahrt
+}
+
+
+
+
 export type LoginResource = {
     /** The JWT */
     "access_token": string,
