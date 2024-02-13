8b5f4978cf4c0c00a7ca3ed31dda9289a37aae73
s88475 <s88475@bht-berlin.de>
2024-02-13 14:06:08 +0100

    UserModel sollte jetzt fertig sein.

---
+++ backend/src/db/UserModel.ts
@@ -0,0 +1,54 @@
+import { Model, model, Schema } from "mongoose";
+import { hash, compare } from "bcryptjs";
+
+/**
+ * Interface with the appointed properties
+ */
+export interface IUser {
+    name: string
+    nachname: string
+    password: string
+    admin?: boolean
+    createdAt?: Date
+    fahrzeuge: {
+        datum: Date;
+        kennzeichen: string;
+    }[];
+}
+
+export interface IUserMethods {
+    isPasswordCorrect(c: string): Promise<boolean>
+}
+
+type UserModel = Model<IUser, {}, IUserMethods>;
+
+
+const userSchema = new Schema<IUser, IUserMethods>({
+    name: { type: String, required: true },
+    nachname: { type: String, required: true },
+    password: { type: String, required: true },
+    admin: { type: Boolean, default: false },
+    createdAt: { type: Date },
+    fahrzeuge: [{
+        datum: { type: Date, required: true },
+        kennzeichen: { type: String, required: true }
+    }]
+}, { timestamps: true });
+
+userSchema.method("isPasswordCorrect", async function (passwordCandidate: string): Promise<boolean> {
+    if (this.isModified()) {
+        throw new Error("User has been modified");
+    }
+    const result = await compare(passwordCandidate, this.password);
+    return result;
+})
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        const hashedPassword = await hash(this.password, 10);
+        this.password = hashedPassword;
+    }
+    next();
+});
+
+export const User = model<IUser, UserModel>("User", userSchema);
