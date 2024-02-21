73e37f9e4edb67e85f90dac8c1ac7403381d7e7c
s88475 <s88475@bht-berlin.de>
2024-02-21 15:42:51 +0100

    Checkpoint hier funktioniert.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,29 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import Loading from './Components/LoadingIndicator';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { RouterProvider, createBrowserRouter } from "react-router-dom";
-// import Login from './Components/Login';
-// import Home from './Components/Home';
-// import FahrtErstellen from './Components/FahrtErstellen';
-
-// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-//   <React.StrictMode>
-//       <RouterProvider 
-//           router={createBrowserRouter([{
-//               path: "/",
-//               children: [
-//                   { path: "", element: <Login /> },
-//                   { path: "home", element: <Home /> },
-//                   { path: "test", element: <Loading /> },
-//               ],
-//             //   errorElement: <App /> /* TODO: create a nice looking error page */
-//           }])}
-//           fallbackElement={<Loading />}
-//       />
-//   </React.StrictMode>
-// );
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -31,8 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './Components/Login';
 import Home from './Components/Home';
-import FahrtErstellen from './Components/FahrtErstellen';
 import Loading from './Components/LoadingIndicator';
+import FahrtErstellen from './Components/FahrtErstellen';
 
 ReactDOM.render(
   <React.StrictMode>
