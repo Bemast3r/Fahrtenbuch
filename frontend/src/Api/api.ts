c277547326b155d9c6785b8114bf1b6bbdf66e12
Can Pala <s88011@bht-berlin.de>
2024-02-23 14:38:05 +0100

    Fahrt erstellen perfekt, außer das Startpunkt in MongoDB nicht angezeigt wird und das die Validierung nicht angezeigt wird wenn man Text schreibt

--- frontend/src/Api/api.ts
+++ frontend/src/Api/api.ts
@@ -96,10 +96,11 @@ export async function getUser(userID: string): Promise<UserResource> {
 
 export async function postFahrt(fahrt: FahrtResource): Promise<FahrtResource> {
     try {
+        const jwt2 = getJWT();
         const response = await fetch(`http://localhost:5000/api/fahrt/user/fahrt/erstellen`, {
             method: "POST",
             headers: {
-                "Authorization": `Bearer ${jwt}`,
+                "Authorization": `Bearer ${jwt2}`,
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({ fahrerid: fahrt.fahrerid, kennzeichen: fahrt.kennzeichen, kilometerstand: fahrt.kilometerstand, startpunkt: fahrt.startpunkt })
