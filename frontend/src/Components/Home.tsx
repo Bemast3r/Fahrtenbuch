be9ba944e9113df4a5974873c2ca15ea6febeed8
s88475 <s88475@bht-berlin.de>
2024-03-04 17:07:02 +0100

    Merged.

--- frontend/src/Components/Home.tsx
+++ frontend/src/Components/Home.tsx
@@ -38,14 +38,11 @@ const Home = () => {
                     <Button variant="primary" type="submit" className="submit-button2" onClick={() => navigate("/verwalten")}>
                         Fahrt verwalten
                     </Button>
-                    <Button variant="danger" type="button" className="submit-button3" onClick={handleAbmelden}>
-                        Abmelden
-                    </Button>
                     <Button variant="primary" type="submit" className="submit-button2" onClick={() => { navigate("/user-erstellen") }}>
                         Benutzer erstellen
                     </Button>
-                    <Button variant="danger" type="submit" className="submit-button3">
-                        Fahrt beenden
+                    <Button variant="danger" type="button" className="submit-button3" onClick={handleAbmelden}>
+                        Abmelden
                     </Button>
                 </Form>
             </div>
