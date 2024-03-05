4e5529b3142ff82f5e8e912e4e2deb01b78b05fd
Can Pala <s88011@bht-berlin.de>
2024-03-05 16:51:29 +0100

    Statistik ansatz

--- frontend/src/Components/Statistik.tsx
+++ frontend/src/Components/Statistik.tsx
@@ -3,8 +3,8 @@ import Row from 'react-bootstrap/Row';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getJWT, setJWT, getLoginInfo } from './Logincontext';
-import { getUser } from '../Api/api';
-import { UserResource } from '../util/Resources';
+import { getFahrt, getUser } from '../Api/api';
+import { FahrtResource, UserResource } from '../util/Resources';
 
 const Statistik = () => {
     const [user, setUser] = useState<UserResource | null>(null)
@@ -27,16 +27,46 @@ const Statistik = () => {
         setUser(user)
     }
 
+    // async function handlePostLenkzeit() {
+    //     const fahrtResource: FahrtResource = {
+    //         fahrerid: usercontexte.id!,
+    //         id: letzteFahrt._id!.toString(),
+    //         _id: letzteFahrt._id!.toString(),
+    //         kennzeichen: letzteFahrt.kennzeichen.toString(),
+    //         kilometerstand: letzteFahrt.kilometerstand,
+    //         startpunkt: letzteFahrt.startpunkt.toString(),
+    //         lenkzeit: [{ start: lenkzeitRecord.start, stop: lenkzeitRecord.stop! }],
+    //         beendet: false,
+    //       };
+          
+    //     const fahrt = await getFahrt(fahrtResource);
+    // }
+
+    const totalTrips = 1000;
+    const ongoingTrips = 300;
+    const completedTrips = 700;
+
+    // Dummy-Daten für Benutzer
+    const totalUsers = 50;
+    const adminUsers = 5;
+    const regularUsers = totalUsers - adminUsers;
+
     useEffect(() => { load() }, [])
 
     return (
         <div className="form-wrapper">
             <h2 className="form-header">Statistiken</h2>
-            <div className="form-container">
-                <Form>
-                    <Row className="mb-1">
-                    </Row>
-                </Form>
+            <div>
+                <h2>Fahrten</h2>
+                <p>Gesamtzahl der Fahrten: {totalTrips}</p>
+                <p>Aktuelle Fahrten: {ongoingTrips}</p>
+                <p>Abgeschlossene Fahrten: {completedTrips}</p>
+            </div>
+            <div>
+                <h2>Benutzer</h2>
+                <p>Gesamtzahl der Benutzer: {totalUsers}</p>
+                <p>Admin-Benutzer: {adminUsers}</p>
+                <p>Reguläre Benutzer: {regularUsers}</p>
             </div>
         </div>
     );
