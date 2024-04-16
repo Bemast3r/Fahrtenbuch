012391358fe1c945796b143accbf0d26cf94a7ec
s88475 <s88475@bht-berlin.de>
2024-04-16 17:31:00 +0200

    Vercel.json lol

--- backend/index.ts
+++ backend/index.ts
@@ -21,13 +21,18 @@ app.use("*", cors({
         "https://fahrtenbuch-frontend.vercel.app/"
     ]
 }));
-app.use(function (request, response, next) {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    response.header("Access-Control-Expose-Headers", "Authorization");
-    next();
-});
+// app.use(function (request, response, next) {
+//     response.header("Access-Control-Allow-Origin", "*");
+//     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     response.header("Access-Control-Expose-Headers", "Authorization");
+//     next();
+// });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://fahrtenbuch-frontend.vercel.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
 app.use(express.json({ limit: "5mb" }));
