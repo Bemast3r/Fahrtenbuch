aff9d729bfdd2dc6b6328ec38f943145b1a20cf7
s88475 <s88475@bht-berlin.de>
2024-04-16 14:25:31 +0200

    Keine Header mehr in index.

--- backend/index.ts
+++ backend/index.ts
@@ -25,12 +25,12 @@ const app = express();
 // }));
 app.use(cors())
 
-app.use(function (request, response, next) {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    response.header("Access-Control-Expose-Headers", "Authorization");
-    next();
-});
+// app.use(function (request, response, next) {
+//     response.header("Access-Control-Allow-Origin", "*");
+//     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     response.header("Access-Control-Expose-Headers", "Authorization");
+//     next();
+// });
 
 app.use(express.json({ limit: "5mb" }));
 app.use(bodyParser.json());
