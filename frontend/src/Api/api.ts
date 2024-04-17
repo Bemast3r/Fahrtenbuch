b44094b9b1b5404a2d4ba7388f0978c8ac75f74f
s88475 <s88475@bht-berlin.de>
2024-04-17 15:53:03 +0200

    Logintest

--- frontend/src/Api/api.ts
+++ frontend/src/Api/api.ts
@@ -11,7 +11,7 @@ export async function login(loginData: { username: string, password: string }): 
     if (!loginData.password)
         throw new Error("password not defined");
 
-    const response = await fetch(`${BASE_URL}/api/login/`, {
+    const response = await fetch("https://fahrtenbuch-backend-latest.vercel.app/api/login/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
